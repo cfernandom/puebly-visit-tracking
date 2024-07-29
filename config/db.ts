d9b2aa69ca9527d54284b8f50bbda9eb660a3728
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-07-29 15:46:32 -0500

    refactor

--- config/db.ts
+++ config/db.ts
@@ -1,13 +1,12 @@
 import postgres from "https://deno.land/x/postgresjs@v3.4.4/mod.js";
 import env from "./environment.ts";
 
-const sql = postgres("", {
+const sql = postgres({
   user: env.DB_USER,
   port: Number(env.DB_PORT),
   database: env.DB_NAME,
   password: env.DB_PASS,
   hostname: env.DB_HOST,
-  publications: "alltables",
 });
 
 async function testConnection() {
