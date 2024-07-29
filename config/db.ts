0454769059ae660028251f48cc5d66e1b1513c71
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-07-29 15:56:53 -0500

    feat: agregar variables de entorno para deno deploy

--- config/db.ts
+++ config/db.ts
@@ -2,11 +2,11 @@ import postgres from "https://deno.land/x/postgresjs@v3.4.4/mod.js";
 import env from "./environment.ts";
 
 const sql = postgres({
-  user: env.DB_USER,
-  port: Number(env.DB_PORT),
-  database: env.DB_NAME,
-  password: env.DB_PASS,
-  hostname: env.DB_HOST,
+  user: env.DB_USER || Deno.env.get("DB_USER"),
+  port: Number(env.DB_PORT) || 5432,
+  database: env.DB_NAME || Deno.env.get("DB_NAME"),
+  password: env.DB_PASS || Deno.env.get("DB_PASS"),
+  hostname: env.DB_HOST || Deno.env.get("DB_HOST"),
 });
 
 async function testConnection() {
--- utilities/hmac.ts
+++ utilities/hmac.ts
@@ -3,7 +3,7 @@ import env from "../config/environment.ts";
 
 function validateHmac(uuid: string, receivedHmac: string): boolean {
     const encoder = new TextEncoder();
-    const key = encoder.encode(env.SHARED_SECRET_KEY);
+    const key = encoder.encode(env.SHARED_SECRET_KEY || Deno.env.get("SHARED_SECRET_KEY"));
     const hmac = createHmac("sha256", key);
     hmac.update(uuid, 'utf-8'); // Asegurarse de que el mensaje esté codificado en UTF-8
     const calculatedHmac = hmac.digest('hex'); // Obtener el HMAC en formato hexadecimal
