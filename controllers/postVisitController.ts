953f91d78682ab01d6bb74a3b8647f7db0b3c00f
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-07-29 19:25:07 -0500

    perf: registrar visita utilizando consultas atomicas

--- controllers/postVisitController.ts
+++ controllers/postVisitController.ts
@@ -4,60 +4,38 @@ import kv from "../config/kv.ts";
 import validateHmac from "../utilities/hmac.ts";
 import { IPostVisitLog } from "../interfaces/IPostVisitLog.ts";
 
-const getUserByUuid = async (uuid: string) => {
-  return await sql`
-    SELECT * 
-    FROM users 
-    WHERE uuid = ${uuid}
-  `;
-};
+const billPostVisit = async (uuid: string, post_id: number, title?: string) => {
+  await sql.begin(async (sql) => {
+    await sql`
+      INSERT INTO users (uuid)
+      VALUES (${uuid})
+      ON CONFLICT (uuid) DO NOTHING;
+    `;
 
-const insertUser = async (uuid: string) => {
-  await sql`
-    INSERT INTO users (uuid)
-    VALUES (${uuid})
-  `;
-};
+    await sql`
+      INSERT INTO posts (id, title)
+      VALUES (${post_id}, ${title || 'Sin título'})
+      ON CONFLICT (id) DO NOTHING;
+    `;
 
-const getPostById = async (post_id: number) => {
-  return await sql`
-    SELECT *
-    FROM posts
-    WHERE id = ${post_id}
-  `;
-};
+    const visitedRecently = await sql`
+      SELECT EXISTS (
+          SELECT 1
+          FROM post_user_visits
+          WHERE user_id = ${uuid}
+          AND post_id = ${post_id}
+          AND time > now() - INTERVAL '12 hours'
+      ) AS has_visited;
+    `;
 
-const insertPost = async (post_id: number, post_title: string | undefined) => {
-  await sql`
-    INSERT INTO posts (id, title)
-    VALUES (${post_id}, ${post_title || 'Sin título'})
-  `;
-};
-
-const hasVisitedRecently = async (uuid: string, post_id: number): Promise<boolean> => {
-  const now = new Date();
-  const twelveHoursAgo = new Date(now.getTime() - 12 * 60 * 60 * 1000);
-  const sinceDate = twelveHoursAgo.toISOString();
-
-  const result = await sql`
-    SELECT EXISTS (
-      SELECT 1
-      FROM post_user_visits
-      WHERE user_id = ${uuid}
-        AND post_id = ${post_id}
-        AND time >= ${sinceDate}
-    ) AS visited_recently
-  `;
-  return result[0].visited_recently;
-};
-
-const insertPostVisit = async (uuid: string, post_id: number) => {
-  const now = new Date().toISOString();
-  await sql`
-    INSERT INTO post_user_visits (time, user_id, post_id)
-    VALUES (${now}, ${uuid}, ${post_id})
-  `;
-};
+    if (!visitedRecently[0].has_visited) {
+      await sql`
+        INSERT INTO post_user_visits (time, user_id, post_id)
+        VALUES (now(), ${uuid}, ${post_id});
+      `;
+    }
+  });
+}
 
 const logPostVisitToKv = async (uuid: string, post_id: number) => {
   await kv.atomic()
@@ -78,24 +56,10 @@ export const logPostVisit = async (c: Context) => {
     if (!validateHmac(uuid, hmac)) {
       return c.json({ error: "invalid data" }, 400);
     }
-
-    const user = await getUserByUuid(uuid);
-    if (user.length === 0) {
-      await insertUser(uuid);
-    }
-
-    const post = await getPostById(post_id);
-    if (post.length === 0) {
-      await insertPost(post_id, post_title);
-    }
-
-    // bill post visit
-    const visitedToday = await hasVisitedRecently(uuid, post_id);
-    if (!visitedToday) {
-      await insertPostVisit(uuid, post_id);
-    }
-
+    
     await logPostVisitToKv(uuid, post_id);
+    
+    await billPostVisit(uuid, post_id, post_title);
 
     return c.json({ message: "ok" });
   } catch (error) {
